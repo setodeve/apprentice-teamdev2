be411776e54c19da1c017ce5647d0f9340012c66
setodeve <keinick11@outlook.com>
2024-02-03 03:29:22 +0900

    参加機能修正

--- frontend/app/src/component/EngagementButtons/index.jsx
+++ frontend/app/src/component/EngagementButtons/index.jsx
@@ -1,29 +1,42 @@
-import React,{useEffect, useState} from "react";
+import React,{useState} from "react";
 import "./index.css";
 import WhiteButton from "../WhiteButton";
 import GreenButton from "../GreenButton";
 import Like from "../Like";
+import axios from "axios";
 
 const EngagementButtons = (props) =>{
-  const [joinState,setjoinState] = useState(false)
-  function handleClick(e){
-    e.preventDefault();
-    setjoinState(!joinState)
+  const [joinState,setjoinState] = useState(props.join)
+  async function handleClick(e){
+    e.preventDefault();    
+    if(joinState){
+      setjoinState(false)
+      await axios.delete(`http://localhost:8080/api/participant/${props.id}`, {
+        params: {
+          "user_id":1, /* TODO: 現在のユーザーを取得するようにする */
+          "otyakai_id":props.id
+        }
+      });
+    }else{
+      setjoinState(true)
+      await axios.post(`http://localhost:8080/api/participant/`, {
+          "user_id":1, /* TODO: 現在のユーザーを取得するようにする */
+          "otyakai_id":props.id
+      });
+
+    }
   }
 
-  useEffect(()=>{
-    
-  },[joinState])
   return (
     <>
       <div className="right-info">
         <Like />
         <span onClick={handleClick}>
         {
-          !joinState ?
-            <WhiteButton name="行く?"/>
-          :
+          joinState ?
             <GreenButton name="行きます!!"/>
+          :
+            <WhiteButton name="行く?"/>
         }
         </span>
 
