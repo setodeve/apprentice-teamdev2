dc079f36a1d045b45ee3e4a7e2d9cae41c3e2031
Hara Misaki <m.kevavu05@gmail.com>
2024-02-02 12:03:11 +0900

    create show controller

--- backend/app/controllers/api/user_controller.rb
+++ backend/app/controllers/api/user_controller.rb
@@ -1,3 +1,20 @@
 class Api::UserController < ApplicationController
+  before_action :set_current_user, only: [:show, :update]
 
+  def index
+    render json: {test: "hello world"}
+  end
+
+  def show 
+    render json: @current_user
+  end
+
+
+
+
+
+  private 
+  def set_current_user 
+    @current_user = User.find(params[:id])
+  end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   namespace :api do
     resources :test
     resources :otyakais
+    resources :user
   end
 end
