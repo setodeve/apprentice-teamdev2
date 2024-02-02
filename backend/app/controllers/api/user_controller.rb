e48ce73a2b57b3276ca872acdc35ee520af7e77c
Hara Misaki <m.kevavu05@gmail.com>
2024-02-02 13:00:46 +0900

    add controllers : create & destroy

--- backend/app/controllers/api/user_controller.rb
+++ backend/app/controllers/api/user_controller.rb
@@ -1,15 +1,31 @@
 class Api::UserController < ApplicationController
-  before_action :set_current_user, only: [:show, :update]
+  before_action :set_current_user, only: [:show, :update, :destroy]
 
   def index
     @users = User.all
     render json: @users
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      render json: @user, status: :created
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
   def show 
     render json: @user
   end
 
+  def update
+
+  end
+
+  def destroy
+    @user.destroy
+  end
 
 
 
@@ -17,5 +33,9 @@ class Api::UserController < ApplicationController
   private 
   def set_current_user 
     @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:id, :email, :password, :name, :profile, :gender, :age, :img)
   end
 end
