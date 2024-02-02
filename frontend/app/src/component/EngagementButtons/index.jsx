23dd77287c985a271989a37f228bdb8f5e11142e
setodeve <keinick11@outlook.com>
2024-02-02 20:51:59 +0900

    参加ボタン修正

--- frontend/app/src/component/EngagementButtons/index.jsx
+++ frontend/app/src/component/EngagementButtons/index.jsx
@@ -1,14 +1,32 @@
-import React from "react";
+import React,{useEffect, useState} from "react";
 import "./index.css";
 import WhiteButton from "../WhiteButton";
+import GreenButton from "../GreenButton";
 import Like from "../Like";
 
 const EngagementButtons = (props) =>{
+  const [joinState,setjoinState] = useState(false)
+  function handleClick(e){
+    e.preventDefault();
+    setjoinState(!joinState)
+  }
+
+  useEffect(()=>{
+    
+  },[joinState])
   return (
     <>
       <div className="right-info">
         <Like />
-        <WhiteButton name="行く"/>
+        <span onClick={handleClick}>
+        {
+          !joinState ?
+            <WhiteButton name="行く?"/>
+          :
+            <GreenButton name="行きます!!"/>
+        }
+        </span>
+
       </div>
     </>
   )
