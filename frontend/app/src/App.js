e83a8d8876a389b11e5c5f875aa02161c338bb54
setodeve <keinick11@outlook.com>
2024-02-04 01:32:41 +0900

    不要な処理削除

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -1,6 +1,4 @@
-// import logo from './logo.svg';
 import './App.css';
-import axios from "axios";
 import { Routes, Route } from 'react-router-dom';
 import React, { useState } from 'react';
 import Header from './component/Header/Header';
@@ -8,30 +6,10 @@ import OtyakaiList from './component/OtyakaiList';
 import OtyakaiShow from './component/OtyakaiShow';
 import CreateOtyakaiPage from './component/CreateOtyakaiPage/createOtyakaiPage';
 function App() {
-  const testMethod = () => {
-    axios
-      .get(`${"http://localhost:8080"}/api/test`, {
-        params: {
-          name: "test"
-        }
-      })
-      .then((res) => {
-        if (res.status === 200) {
-          console.log(res.data);
-        } else {
-          console.log("失敗しました");
-        }
-      })
-      .catch(() => {
-        console.log("catch");
-      });
-  }
-  
   return (
     <>
       <HeaderInfo/>
       <Routes>
-        {/* <Route path="/" element={<button onClick={() => testMethod()}></button>} /> */}
         <Route path="/login" element={<div>login</div>} />
         <Route path="/signup" element={<div>signup</div>} />
         <Route path="/" element={<OtyakaiList/>} />
