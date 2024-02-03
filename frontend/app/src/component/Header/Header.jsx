53255b50b3ce25fff52d55a0ade1fee7b322d652
Hara Misaki <m.kevavu05@gmail.com>
2024-02-03 16:47:50 +0900

    implement header

--- frontend/app/src/component/Header/Header.jsx
+++ frontend/app/src/component/Header/Header.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import './Header.css';
 import { Link } from 'react-router-dom';
 import SearchBar from '../Search-bar/search-bar';
+import GreenButton from '../GreenButton';
+import WhiteButton from '../WhiteButton';
 
 const Header = ({ isLoggedIn, onLogout }) => {
   return (
@@ -13,13 +15,16 @@ const Header = ({ isLoggedIn, onLogout }) => {
         {isLoggedIn ? (
           <ul>
             <li><SearchBar /></li>
-            <li><Link to="/profile">My Profile</Link></li>
-            <li><a href="/" onClick={onLogout}>Logout</a></li>
+            <li><Link to="/profile"><GreenButton name="My Profile"/></Link></li>
+            <li><Link to="/"><GreenButton name="Log Out"/></Link> </li>
           </ul>
         ) : (
           <ul>
-            <li><Link to="/signup">Signup</Link></li>
-            <li><Link to="/login">Logout</Link></li>
+            {/* <li><Link to="/signup">Signup</Link></li>
+            <li><Link to="/login">Logout</Link></li> */}
+
+            <li><GreenButton><Link to="/signup">Signup</Link></GreenButton></li>
+            <li><GreenButton><Link to="/login">Logout</Link></GreenButton></li>
           </ul>
         )}
       </nav>
