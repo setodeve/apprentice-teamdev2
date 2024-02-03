bec40203676844bbbeefd2c2ea4bccf0ea9583cb
setodeve <keinick11@outlook.com>
2024-02-03 19:58:08 +0900

    onChange関数追加

--- frontend/app/src/component/textArea/textArea.jsx
+++ frontend/app/src/component/textArea/textArea.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./textArea.css";
 
-export default function TextArea({label="label Propsに値を与えてください", defaultValue=""}) {
+export default function TextArea({label="label Propsに値を与えてください", defaultValue="",func=()=>{}}) {
 
   return (
     <>
       <div className="input-area">
         <span className="input-label">{label}</span>
-        <textarea className="shadow" defaultValue={defaultValue} />
+        <textarea className="shadow" defaultValue={defaultValue} onChange={func} />
       </div>
     </>
   );
